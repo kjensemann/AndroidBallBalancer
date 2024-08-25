5a13ecd7385b675f6c34060ddf275872647b77bb
Kristian Holm Jensen <kristian.holm.jensen@gmail.com>
2024-08-25 22:53:08 +0200

    General update to gradle etc

--- app/src/main/java/com/example/avr_ballbalancerapp/clsBallControllerData.java
+++ app/src/main/java/com/example/avr_ballbalancerapp/clsBallControllerData.java
@@ -5,6 +5,7 @@ public class clsBallControllerData {
     private float Ki, Kd, Kp; //Integral, Derivative and Proportional constants.
     private float KV_Arr[], PV_Arr[];
     private float SP; //SetPoint
+    private String dataNameStr;
     /* --------------
      Class to hold data received from ESP8266 over WiFi and to make it plottable.
         - PV Data, CV data and SetPoint kept here
